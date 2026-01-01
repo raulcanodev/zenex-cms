d18e8cb142e7e68faf9f31ff4f29804ce82380da
Raul Marco Cano <rawraul@outlook.com>
2026-01-01 18:01:55 +0100

    feat: update layout header to include GitHub link and improve styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,29 +36,6 @@ export default function RootLayout({
           enableSystem={false}
           disableTransitionOnChange
         >
-          <header className="flex justify-between items-center p-4 bg-gray-100">
-            <h1 className="text-xl font-bold">{config.appName}</h1>
-            <a
-              href="https://github.com/raulcanodev/zenex-cms"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-700 hover:text-black"
-              aria-label="GitHub Repository"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12 2C6.477 2 2 6.477 2 12c0 4.418 2.865 8.166 6.839 9.49.5.092.682-.217.682-.483 0-.237-.009-.868-.014-1.703-2.782.603-3.369-1.342-3.369-1.342-.454-1.154-1.11-1.461-1.11-1.461-.908-.62.069-.608.069-.608 1.004.07 1.532 1.032 1.532 1.032.892 1.528 2.341 1.087 2.91.832.092-.647.35-1.087.636-1.338-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.983 1.03-2.682-.103-.253-.447-1.27.098-2.645 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0 1 12 6.845c.85.004 1.705.115 2.504.337 1.91-1.296 2.75-1.026 2.75-1.026.545 1.375.2 2.392.098 2.645.64.7 1.03 1.591 1.03 2.682 0 3.842-2.337 4.687-4.565 4.937.36.31.682.92.682 1.852 0 1.337-.012 2.419-.012 2.747 0 .268.18.579.688.481A10.003 10.003 0 0 0 22 12c0-5.523-4.477-10-10-10z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </a>
-          </header>
           <SessionProvider>{children}</SessionProvider>
         </ThemeProvider>
       </body>
